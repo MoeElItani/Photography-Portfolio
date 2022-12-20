efaf3a9d506fb8cd5c0b1b88f592d2129ad4998d
MoeElItani <mhmd98itani@gmail.com>
2022-12-20 21:37:23 +0200

    minor improvements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='h-[15vh] bg-[#1F2022] flex flex-col justify-center content-center text-center text-white'>
+    <footer className='pt-[65vh] md:pt-0 h-[75vh] md:h-[15vh] bg-[#1F2022] flex flex-col justify-center content-center text-center text-white z-50'>
             <p className='font-semibold my-[0.5rem]'>
                &copy; 2022 Mohammad El Itani | All Rights
                Reserved{' '}
